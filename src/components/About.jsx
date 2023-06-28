6c862dc82ad251b9caec0c7d4b70ce6cd1c8c686
Fel1peLima <zueragamer61@gmail.com>
2023-06-28 20:43:29 -0300

    portfolio

---
+++ src/components/About.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="sobre"
+    className="w-full h-screen bg-gradient-to-b from-blue-950  to-black text-white"
+    >
+        <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center
+        w-full h-full">
+            <div className="pb-8">
+                <p className="text-4xl font-bold inline border-b-4 border-white">
+                    Sobre Mim
+                </p>
+            </div>
+            <p className="text-xl mt-20">
+            Olá meu nome é Felipe Batista de Lima! Sou um desenvolvedor front-end apaixonado pelo desenvolvimento web e design, tenho um grande conhecimento em HTML, CSS, JavaScript e tecnologias em NodeJS, ReactJS, MongoDB, MySQL e WordPress, sempre buscando conhecimento para melhorar a cada dia.
+            </p>
+            <br />
+            <p className="text-xl">
+            Com sólidos conhecimentos em HTML, CSS e JavaScript, estou bem equipado para transformar conceitos de design em interfaces web visualmente atraentes e intuitivas. Tenho experiência no desenvolvimento responsivo, garantindo que meus projetos sejam acessíveis e funcionem perfeitamente em diferentes dispositivos e tamanhos de tela.
+            </p>
+            <br />
+            <p className="text-xl">
+            <p className="text-xl">
+            Se você procura um desenvolvedor front-end comprometido, motivado e apaixonado, estou pronto para 
+            colaborar em projetos empolgantes. Sinta-se à vontade para explorar meu portfólio e entrar em contato 
+            comigo para discutir como podemos trabalhar juntos para alcançar seus objetivos.
+            </p>
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/Home.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import Minhaimg from"../assets/minha-foto.jpg"
+import {MdOutlineKeyboardArrowRight} from "react-icons/md"
+import {Link} from "react-scroll";
+
+const Home = () => {
+  return (
+    <div name="home"
+    className="h-screen w-full bg-gradient-to-b from-black via-black to-blue-950"
+    >
+        <div className="max-w-screen-lg mx-auto flex flex-col items-center
+        justify-center h-full px-4 md:flex-row">
+            <div className="flex flex-col justify-center h-full  bg-gradient-to-r from-blue-500 to-purple-900 text-transparent bg-clip-text">
+                <h2 className=" text-4xl sm:text-7xl font-bold ">
+                    Desenvolvedor Front-End</h2>
+                    <p className="text-gray-300 py-4 max-w-md">
+                    Sou um Desenvolvedor Front-End apaixonado por criar experiência digitais. Com uma sólida formação em desenvolvimento web e uma paixão por design, tenho trabalhando incansavelmente e mater-me atualizado com as últimas tendências e tecnologias.
+
+                    </p>
+                    <div>
+                        <Link to="portfolio"
+                        smooth
+                        duration={500}
+                        className="group text-white w-fit px-6 py-3 my-2 flex
+                        items-center rounded-md bg-gradient-to-r from-blue-700
+                        to-blue-950 cursor-pointer">
+                            Portfolio
+                            <span className="group-hover:rotate-90 duration-300">
+                            <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
+                            </span>
+                        </Link>
+                    </div>
+            </div>
+            <div>
+                <img src={Minhaimg} alt="meu perfil" 
+                className="ml-5 rounded-2xl mx-auto w-2/3 md:w-full " />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
